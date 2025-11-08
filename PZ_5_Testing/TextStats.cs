136f995d3cea13018d66ac745d132a5f3709e13b
nerobeev.an2215 <nerobeev.an2215@PC-6-ROOM410>
2025-11-08 10:43:46 +0300

    Добавьте файлы проекта.

---
+++ PZ_5_Testing/TextStats.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PZ_5_Testing
+{
+    public class TextStats
+    {
+        public int wc { get; set; }
+        public string lw { get; set; }
+        public Dictionary<char, int> cf { get; set; }
+    }
+}
