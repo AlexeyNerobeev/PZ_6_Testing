e49437134fd3da14de48933f7ce0da28ff4ffbb0
kostylev.as2210 <kostylev.as2210@C0M207L6>
2025-11-13 16:15:51 +0500

    улучшение архитектуры TextFormatter

---
+++ PZ_5_Testing/FormatOptions.cs
@@ -0,0 +1,20 @@
+﻿using PZ_5_Testing;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PZ_6_Testing
+{
+    /// <summary>
+    /// Класс для настройки параметров форматирования
+    /// </summary>
+    public class FormatOptions
+    {
+        public bool RemoveExtraSpaces { get; set; }
+        public bool ConvertToUpper { get; set; }
+        public bool JustifyText { get; set; }
+        public int LineWidth { get; set; } = TextFormatter.DefaultLineWidth;
+    }
+}
